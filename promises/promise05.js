6c486756423b0c64022d22914031cd868dfb58a5
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-02-01 02:04:50 -0300

    Update promises

--- promises/promise05.js
+++ promises/promise05.js
@@ -8,6 +8,21 @@ function treatError(value, error) {
     });
 }
 
+function errorCatcher(value) {
+    return new Promise(function(resolve, reject) {
+        try {
+            consol.log('loading...');
+            resolve(value);
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
 treatError('Testing...', 0.9)
     .then(console.log)
     .catch(err => console.log(`Error: ${err}`));
+
+errorCatcher('Testing')
+    .then(console.log)
+    .catch(err => console.error(`Error: ${err}`));
